887345f638d47f77ab416253b834990a83f4dff0
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-02-11 13:53:13 +0100

    Initial commit

---
+++ src/main/java/sabatinoprovenza/BE_S6_L3/BeS6L3Application.java
@@ -0,0 +1,13 @@
+package sabatinoprovenza.BE_S6_L3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeS6L3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeS6L3Application.class, args);
+	}
+
+}
